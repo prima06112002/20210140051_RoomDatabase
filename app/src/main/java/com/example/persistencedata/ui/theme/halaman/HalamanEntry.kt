c145f947f6dc9582fcbe5305c9668c703c674bb8
Prima06112002 <ramadhan06112002@gmail.com>
2023-12-06 19:37:50 +0700

    menambahkan outlinedteks untuk nama

--- app/src/main/java/com/example/persistencedata/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/persistencedata/ui/theme/halaman/HalamanEntry.kt
@@ -2,12 +2,16 @@ package com.example.persistencedata.ui.theme.halaman
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.persistencedata.R
 import com.example.persistencedata.model.DetailSiswa
@@ -43,6 +47,14 @@ fun FormInputSiswa(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
     ) {
+        OutlinedTextField(
+            value = detailSiswa.nama,
+            onValueChange ={onValueChange(detailSiswa.copy(nama = it))},
+            label = { Text(stringResource(R.string.nama)) },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled
+        )
 
     }
 }
