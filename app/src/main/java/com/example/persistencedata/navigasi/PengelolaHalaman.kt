280515d256e3077246ed101f205ee7eca97ae4e8
Prima06112002 <ramadhan06112002@gmail.com>
2023-12-06 19:59:45 +0700

    Menambahkan NavHost untuk function HostNavigasi di PengelolaHalaman

--- app/src/main/java/com/example/persistencedata/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/persistencedata/navigasi/PengelolaHalaman.kt
@@ -12,7 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.example.persistencedata.R
+import com.example.persistencedata.ui.theme.halaman.DestinasiEntry
+import com.example.persistencedata.ui.theme.halaman.DestinasiHome
+import com.example.persistencedata.ui.theme.halaman.EntrySiswaScreen
+import com.example.persistencedata.ui.theme.halaman.HomeScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -44,5 +50,18 @@ fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route)})
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
 
 }
