491166d396bb081732d6d7f35f8868189998e2c4
Prima06112002 <ramadhan06112002@gmail.com>
2023-12-06 18:48:00 +0700

    Membuat data class HomeUIState

--- app/src/main/java/com/example/persistencedata/model/HomeViewModel.kt
+++ app/src/main/java/com/example/persistencedata/model/HomeViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.persistencedata.model
 
 import androidx.lifecycle.ViewModel
+import com.example.persistencedata.data.Siswa
 import com.example.persistencedata.repos.RepositorySiswa
 
 class HomeViewModel(private val repositoriSiswa: RepositorySiswa) : ViewModel() {
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
 }
