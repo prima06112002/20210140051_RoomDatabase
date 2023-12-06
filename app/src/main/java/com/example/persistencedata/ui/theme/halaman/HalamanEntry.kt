19ad89fb86236d747d065d97149ec9d202378413
Prima06112002 <ramadhan06112002@gmail.com>
2023-12-06 19:20:33 +0700

    Menambahkan function EntrySiswaScreen

--- app/src/main/java/com/example/persistencedata/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/persistencedata/ui/theme/halaman/HalamanEntry.kt
@@ -1,9 +1,25 @@
 package com.example.persistencedata.ui.theme.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.persistencedata.R
+import com.example.persistencedata.model.EntryViewModel
+import com.example.persistencedata.model.PenyediaViewModel
 import com.example.persistencedata.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
 }
