f18ea6f153ad5719935af993f4f733a1f55c9b80
Prima06112002 <ramadhan06112002@gmail.com>
2023-12-06 19:35:36 +0700

    membuat FormInputSiswa di HalamanEntry

--- app/src/main/java/com/example/persistencedata/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/persistencedata/ui/theme/halaman/HalamanEntry.kt
@@ -1,12 +1,16 @@
 package com.example.persistencedata.ui.theme.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.persistencedata.R
+import com.example.persistencedata.model.DetailSiswa
 import com.example.persistencedata.model.EntryViewModel
 import com.example.persistencedata.model.PenyediaViewModel
 import com.example.persistencedata.navigasi.DestinasiNavigasi
@@ -26,4 +30,19 @@ fun EntrySiswaScreen(
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+
+    }
 }
